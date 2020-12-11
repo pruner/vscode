18b9edccb4cda71300c0e8ca6d5aa9b08c92a305
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-12-11 12:17:26 +0100

    fix for extension.

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,38 @@ import { clearRenderedCoverage, renderCoverage } from './render';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	vscode.window.onDidChangeActiveTextEditor(renderCoverage);
-	await renderCoverage();
+    const baseOptions: vscode.DecorationRenderOptions = {
+        isWholeLine: true,
+        gutterIconSize: "contain"
+    };
+
+    const succeededType = vscode.window.createTextEditorDecorationType({
+        ...baseOptions,
+        gutterIconPath: context.asAbsolutePath('resources/succeeded.svg'),
+        backgroundColor: 'rgba(0, 255, 0, 0.01)'
+    });
+	context.subscriptions.push(succeededType);
+
+    const failedType = vscode.window.createTextEditorDecorationType({
+        ...baseOptions,
+        gutterIconPath: context.asAbsolutePath('resources/failed.svg'),
+        backgroundColor: 'rgba(255, 0, 0, 0.01)'
+	});
+	context.subscriptions.push(failedType);
+
+	const onRender = () => 
+		renderCoverage(context, failedType, succeededType);
+	
+	const watcher = vscode.workspace.createFileSystemWatcher('**/.pruner/state/*', false, false, false);
+	context.subscriptions.push(watcher.onDidChange(onRender));
+	context.subscriptions.push(watcher.onDidCreate(onRender));
+	context.subscriptions.push(watcher.onDidDelete(onRender));
+	context.subscriptions.push(watcher);
+
+	const onDidChangeActiveTextEditor = vscode.window.onDidChangeActiveTextEditor(onRender);
+	context.subscriptions.push(onDidChangeActiveTextEditor);
+
+	await onRender();
 }
 
 // this method is called when your extension is deactivated
--- src/render.ts
+++ src/render.ts
@@ -4,7 +4,11 @@ import * as vscode from 'vscode';
 
 import { getStatesInWorkspace } from "./state";
 
-export async function renderCoverage() {
+export async function renderCoverage(
+    context: vscode.ExtensionContext,
+    failedType: vscode.TextEditorDecorationType,
+    succeededType: vscode.TextEditorDecorationType) 
+{
     const activeEditor = vscode.window.activeTextEditor;
     if(!activeEditor) {
         return;
@@ -20,55 +24,29 @@ export async function renderCoverage() {
         return;
     }
 
+    const tests = _.flatMap(states, x => x.tests);
+
     const coverageForFile = _
         .flatMap(states, x => x.coverage)
         .filter(l => l.fileId === file.id);
 
-    const whitespaceType = vscode.window.createTextEditorDecorationType({
-        before: {
-            backgroundColor: 'rgba(0,0,0,0)',
-            contentText: " ",
-        }
-    });
-
-    const succeededType = vscode.window.createTextEditorDecorationType({
-        before: {
-            backgroundColor: 'green',
-            contentText: " ",
-        }
-    });
-    const failedType = vscode.window.createTextEditorDecorationType({
-        before: {
-            backgroundColor: 'red',
-            contentText: " ",
-        }
-    });
-
-    activeEditor.setDecorations(whitespaceType, [
-        new vscode.Range(
-            new vscode.Position(0, 0),
-            new vscode.Position(0, 1000))
-    ]);
-
-    activeEditor.setDecorations(whitespaceType, [
-        new vscode.Range(
-            new vscode.Position(1, 0),
-            new vscode.Position(1, 1000))
-    ]);
-
-    activeEditor.setDecorations(whitespaceType, [
-        new vscode.Range(
-            new vscode.Position(3, 0),
-            new vscode.Position(3, 1000))
-    ]);
+    const failedRanges = new Array<vscode.Range>();
+    const succeededRanges = new Array<vscode.Range>();
 
     for(let lineCoverage of coverageForFile) {
-        activeEditor.setDecorations(failedType, [
-            new vscode.Range(
-                new vscode.Position(lineCoverage.lineNumber - 1, 0),
-                new vscode.Position(lineCoverage.lineNumber - 1, 1000))
-        ]);
+        const lineTests = tests.filter(t => lineCoverage.testIds.find(i => i === t.id));
+        const failed = lineTests.find(x => !x.passed);
+        
+        const targetArray = failed ?
+            failedRanges :
+            succeededRanges;
+        targetArray.push(new vscode.Range(
+            new vscode.Position(lineCoverage.lineNumber - 1, 0),
+            new vscode.Position(lineCoverage.lineNumber - 1, 0)));
     }
+    
+    activeEditor.setDecorations(failedType, failedRanges);
+    activeEditor.setDecorations(succeededType, succeededRanges);
 }
 
 export async function clearRenderedCoverage() {
